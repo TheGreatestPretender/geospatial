abc2658f523558caccd868ebd12f6609b615b923
Kat S <kats@MacBook-Pro-101.local>
2021-09-23 16:37:44 -0400

    init commit

---
+++ src/app.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import MapGL from 'react-map-gl';
+import { MapStylePicker } from './controls';
+
+const MAPBOX_STYLE = 'mapbox://styles/mapbox/light-v9';
+const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
+
+function SetToken() {
+  return (
+    <div style={{fontSize: '20px'}}>
+      <div>You don't have a Mapbox token set in your environemnt.</div>
+      <ul>
+        <li>
+          Go to <a href="http://mapbox.com">Mapbox</a> and log in or sign up to
+          get a token.
+        </li>
+        <li>Copy the token to your clipboard.</li>
+        <li>Stop this app in the terminal (ctrl+c)</li>
+        <li>
+          <p>type: </p>
+          <p>
+            <code>export MapboxAccessToken="</code>, then paste your token, then
+            type a closing ".
+          </p>{' '}
+          ie <code>export MapboxAcessToken="pk.123456"</code>
+        </li>
+        <li>
+          Restart the app from the terminal (<code>yarn start</code>)
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      style: 'mapbox://styles/mapbox/light-v9',
+      viewport: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        longitude: -74,
+        latitude: 40.7,
+        zoom: 11,
+        maxZoom: 16
+      }
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this._resize);
+    this._resize();
+  }
+  
+  componentWillUnmount() {
+    window.removeEventListener('resize', this._resize);
+  }
+
+  _resize = () => {
+    this._onViewportChange({
+      width: window.innerWidth,
+      height: window.innerHeight
+    })
+  }
+
+  onStyleChange = style => this.setState({style});
+
+  _onViewportChange(viewport) {
+    this.setState({
+      viewport: {...this.state.viewport, ...viewport}
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <MapStylePicker onStyleChange={this.onStyleChange} currentStyle={this.state.style}/>
+        <MapGL
+          {...this.state.viewport}
+          mapStyle={this.state.style}
+          onStyleChange={this.onStyleChange}
+          onViewportChange={vp => this._onViewportChange(vp)}
+           />
+      </div>
+    );
+  }
+}
